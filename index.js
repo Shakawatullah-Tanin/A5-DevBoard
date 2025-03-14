3895897aa22fcd0f92e39725c43c20bf828bce6c
Shakawatullah-Tanin <shakawatullahtanin23@gmail.com>
2025-03-14 23:31:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("connect to html");
+document.getElementById("nav-btn").addEventListener("click",function(){
+    
+})
+document.getElementById("new-html-page-btn").addEventListener("click",function(){
+    window.location.href="./main.html"
+})
+document.getElementById("com-btn-1").addEventListener("click",function(){
+    alert("board Updated Successfully")
+})
+document.getElementById("date").innerHTML = new Date().toLocaleDateString('en-GB', { 
+    weekday: 'long'
+ }) + ",<br>" + new Date().toLocaleDateString('en-GB', { 
+ month: 'long', day: 'numeric', year: 'numeric' 
+ }).replace(/(\d+) (\w+) (\d+)/,'  $2 $1 $3');
