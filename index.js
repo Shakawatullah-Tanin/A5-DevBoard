aa1a65203806405653a2a1c0954f2a6a22110503
Shakawatullah-Tanin <shakawatullahtanin23@gmail.com>
2025-03-16 08:19:23 +0600

    added button disable

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ document.getElementById("new-html-page-btn").addEventListener("click",function()
 })
 document.getElementById("com-btn-1").addEventListener("click",function(){
     alert("board Updated Successfully")
+    this.disabled =true;
 })
+
 document.getElementById("date").innerHTML = new Date().toLocaleDateString('en-GB', { 
     weekday: 'long'
  }) + ",<br>" + new Date().toLocaleDateString('en-GB', { 
